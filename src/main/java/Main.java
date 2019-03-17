640f419d8843041a9755a741faa3d2fc0dbd6d81
Sebastian <sebastian.aronsson@iths.se>
2019-03-17 14:15:31 +0100

    Change url

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,17 +2,30 @@ import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) throws UnirestException {
 
-        //Simple HTTP GET request
-        HttpResponse<JsonNode> response = Unirest.get("http://httpbin.org/get").queryString("Limit",10).asJson();
+        //Change URL to match your service
 
-        System.out.println(response.getBody().getObject().getString("url"));
-        System.out.println(response.getBody().getObject().getJSONObject("headers").getString("Host"));
+        //HTTP GET request
+        HttpResponse<JsonNode> getResponse = Unirest.get("http://httpbin.org/get").queryString("Limit",10).asJson();
 
+        System.out.println(getResponse.getBody().getObject().getString("url"));
+        System.out.println(getResponse.getBody().getObject().getJSONObject("headers").getString("Host"));
+
+        //Post Request with Json
+        JSONObject exampleJson = new JSONObject().put("name","Oskar");
+        exampleJson.put("age",21);
+        exampleJson.put("id",4);
+        HttpResponse<String> postResponse = Unirest.post("http://httpbin.org/post").body(exampleJson).asString();
+
+        System.out.println(postResponse.getBody());
 
     }
 
